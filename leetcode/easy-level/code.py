4ee59e3676397898d0b5ddbde92e7075d7e210e1
Samandarov Doniyor <doniyorsamandarov069@gmail.com>
2024-08-27 10:45:38 +0500

    added

---
+++ leetcode/easy-level/code.py
@@ -0,0 +1,8 @@
+# 121
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        for index,value in enumerate(prices):
+            
+
+q = Solution()
+print(q.maxProfit([7, 1, 5, 3, 6, 4]))
